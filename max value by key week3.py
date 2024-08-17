5bde42e8c0435f1ff6a3bbc0b5664a3f3b47fb63
sayyedrabeeh <sayyedrabeeh240@gmail.com>
2024-08-17 17:49:32 +0530

    Add files via upload

---
+++ max value by key week3.py
@@ -0,0 +1,13 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+''' 
+li={'a':10,'b':15,'c':20,'d':25}
+l=0
+
+k={key for key,val in li.items() if val==max(li.values())}
+print(k)
